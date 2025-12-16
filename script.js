1856d6b57dfc16f6acef9aecf38fea56f0ffb7b2
VitorSantos0022 <vitorsantos00627@gmail.com>
2025-12-16 00:30:29 -0300

    Correção de redirecionamento e controle de sessão

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword, onAuthStateChanged } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+// LOGIN
 const form = document.getElementById("loginForm");
 
 if (form) {
@@ -21,9 +25,25 @@ if (form) {
   });
 }
 
-// Proteção do admin
+// CONTROLE DE ACESSO
 onAuthStateChanged(auth, (user) => {
-  if (window.location.pathname.includes("admin.html") && !user) {
+
+  // Se estiver logado e estiver no index → manda pro admin
+  if (user && window.location.pathname.endsWith("index.html")) {
+    window.location.href = "admin.html";
+  }
+
+  // Se NÃO estiver logado e estiver no admin → manda pro login
+  if (!user && window.location.pathname.endsWith("admin.html")) {
     window.location.href = "index.html";
   }
+  const logoutBtn = document.getElementById("logout");
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", () => {
+    signOut(auth).then(() => {
+      window.location.href = "index.html";
+    });
+  });
+}
 });
