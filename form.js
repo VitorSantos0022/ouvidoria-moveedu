218ed1122d32c2ec56427aa430e324f8858f85eb
VitorSantos0022 <vitorsantos00627@gmail.com>
2025-12-16 01:59:54 -0300

    Correção geral formulário e admin

--- form.js
+++ form.js
@@ -1,7 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { app } from "./firebase.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBphT72hcN0MJlCmjiNyKOwECoGuNLymrc",
+  authDomain: "ouvidoria--moveedu.firebaseapp.com",
+  projectId: "ouvidoria--moveedu",
+  storageBucket: "ouvidoria--moveedu.firebasestorage.app",
+  messagingSenderId: "928513173800",
+  appId: "1:928513173800:web:79c12bcef0919245b4481d"
+};
+
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const form = document.getElementById("formOuvidoria");
@@ -10,24 +20,20 @@ const status = document.getElementById("status");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const nome = document.getElementById("nome").value;
-  const telefone = document.getElementById("telefone").value;
-  const gosta = document.getElementById("gosta").value;
-  const mensagem = document.getElementById("mensagem").value;
-
   try {
     await addDoc(collection(db, "manifestacoes"), {
-      nome: nome || "Anônimo",
-      telefone: telefone || "Não informado",
-      gosta: gosta || "Não informado",
-      mensagem,
+      nome: form.nome.value || "Anônimo",
+      telefone: form.telefone.value || "Não informado",
+      mensagem: form.mensagem.value,
+      gosta: form.gosta.value || "Não informado",
       data: serverTimestamp()
     });
 
     status.innerText = "Mensagem enviada com sucesso!";
     form.reset();
-  } catch (error) {
+
+  } catch (err) {
+    console.error(err);
     status.innerText = "Erro ao enviar.";
-    console.error(error);
   }
 });
